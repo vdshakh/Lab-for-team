92f5c65565fbfe891cc7419aa382dcdc874f9c9a
RomanKharchenko <RomanKharchenko99@gmail.com>
2020-10-16 16:19:24 +0300

    AbstractFactory, EnumFactories

---
+++ src/com/AbstractFactory.java
@@ -0,0 +1,13 @@
+package com;
+
+public class AbstractFactory {
+    public BaseFactory getFactory(Factories types)
+    {
+        switch (types) {
+            case WhiteFactory: return new WhiteFactory();
+            case BlackFactory: return new BlackFactory();
+            default: throw new IllegalArgumentException("Doesn't exist:" + types);
+        }
+    }
+}
+}
---
+++ src/com/Factories.java
@@ -0,0 +1,6 @@
+package com;
+
+public enum Factories {
+    WhiteFactory,
+    BlackFactory;
+}
